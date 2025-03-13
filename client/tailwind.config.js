158e8f9f1b07ee1a8951b146812b8970627f606d
Dinel <dinel92x@gmail.com>
2025-03-13 13:35:30 +0100

    V6.0 - NodeJS - React - Tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -23,6 +23,6 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    require('@tailwindcss/forms')
   ],
-}
+};
