d3a15048f57e9ce801528cb8df7a3a26333f088a
Dinel <dinel92x@gmail.com>
2025-03-24 16:06:32 +0100

    deleted backup

--- backup/services/prisma.service.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from '../../backup/utils_bak/prisma.utils';
-
-// Re-export the singleton instance
-export { prisma };
-
-// Graceful shutdown
-process.on('beforeExit', async () => {
-  await prisma.$disconnect();
-});
